d53572f3a3418806134e1ef71ba6801177a1f63d
alexanderboyuanzhang <alexander.boyuan.zhang@gmail.com>
2019-07-18 16:55:01 +0200

    .

---
+++ hackerrank/characters.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT
+           int age;
+           char name[30];
+           char temp;
+
+           printf("Enter age: ");
+           scanf("%d",&age);
+           printf("Enter name: ");
+           scanf("%c",&temp); // temp statement to clear buffer
+           fgets(name,30,stdin);
+
+           printf("Name is: %s, age is: %d\n",name,age);
+           return 0;
+         */
+
+        char temp;
+        char ch[10], str[20], sen[100];
+
+        scanf("%c", &temp);
+        fgets(ch, 10, stdin);
+        scanf("%c", &temp);
+        fgets(str, 20, stdin);
+        scanf("%c", &temp);
+        fgets(sen,30,stdin);
+
+        printf("%s\n", ch);
+        printf("%s\n", str);
+        printf("%s", sen);
+
+
+        return 0;
+}
