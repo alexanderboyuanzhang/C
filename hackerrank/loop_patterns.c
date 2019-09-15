ecdeceb6bf7083cfc01efd9166af4f3756593251
alexanderboyuanzhang <alexander.boyuan.zhang@gmail.com>
2019-09-15 20:18:31 +0200

    .

--- hackerrank/loop_patterns.c
+++ hackerrank/loop_patterns.c
@@ -8,19 +8,28 @@ int main()
 
         int n=5;
 
-        for(int i = 1; i<=(n); i++) {
-                int j = 0;
-                for(j=1; j<=(n+n-1); j++) {
-
-                        if(j<=i) {
-                                printf("%d", n-j+1);
-                        }else if(i+j<=n*2)
-                                printf("%d",n-i+1);
-                        else printf("%d",j-n+1 );
+        for(int i = 0; i<=(n+n-2); i++) {
+                for(int j = 0; j<=(n+n-2); j++) {
+                        if(i<n && j<n) {
+                                if(i<j)
+                                        printf("%d ", n-i);
+                                else printf("%d ", n-j);
+                        } else if(i>=n && j<n) {
+                                if((n-1)*2-i<j)
+                                        printf("%d ", n- ((n-1)*2-i) );
+                                else printf("%d ", n-j );
+                        } else if(j>=n && i<n) {
+                                if((n-1)*2-j<i)
+                                        printf("%d ", n- ((n-1)*2-j) );
+                                else printf("%d ", n-i );
+                        }else{
+                                if((n-1)*2-j<(n-1)*2-i)
+                                        printf("%d ", n- ((n-1)*2-j) );
+                                else printf("%d ", n-((n-1)*2-i) );
+                        }
                 }
 
                 printf("\n");
         }
-
         return 0;
 }
